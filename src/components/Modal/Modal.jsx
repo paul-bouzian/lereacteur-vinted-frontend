e36742232cd7cbc243299cb601ed6d9ea08dbbed
Paul Bouzian <paulbouzian@mbp-de-paul.home>
2024-06-13 15:12:51 +0200

    changed modal margin bottom

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,8 +3,8 @@ import Signup from "./Signup";
 
 function Modal({ modal, setModal, setConnected }) {
   return (
-    <div className="absolute left-0 top-0 flex h-[110vh] w-screen items-center justify-center bg-black bg-opacity-50">
-      <div className="relative w-2/5 min-w-96 rounded-lg bg-white p-6 opacity-100">
+    <div className="absolute left-0 top-0 flex h-[105vh] w-screen items-center justify-center bg-black bg-opacity-50">
+      <div className="relative mb-10 w-2/5 min-w-96 rounded-lg bg-white p-6 opacity-100">
         {
           /* <Signup /> */ modal === "signup" && (
             <Signup setModal={setModal} setConnected={setConnected} />
